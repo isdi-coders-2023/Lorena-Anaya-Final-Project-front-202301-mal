eebd8e93c3cb8d2c24efe5a802f166c76b14f26a
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-15 21:15:41 +0100

    add register page

---
+++ src/pages/RegisterPage/RegisterPageStyled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const RegisterPageLogo = styled.h2`
+  font-family: 'Rubik-medium', sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 47px;
+  line-height: 56px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  padding: 1.5rem;
+  color: #fafaff;
+`;
