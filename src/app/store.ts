14c2944bf79fb6f43463da0250bbe17a946b2dbc
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-19 18:48:44 +0100

    add translations reducer in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,12 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import authUserReducer from '../features/auth/auth-slice';
+import translationsReducer from '../features/translations/translations-slice';
+
 export const store = configureStore({
-  reducer: { authUserReducer: authUserReducer },
+  reducer: {
+    authUserReducer: authUserReducer,
+    translationsReducer: translationsReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
