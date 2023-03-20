f899fec6fc498bad48223f2bee0f570a47735245
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-20 12:53:01 +0100

    add height in root and app

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { RouterProvider } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ height: '100vh' }}>
       <RouterProvider router={router} />
     </div>
   );
