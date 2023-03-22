f7d99a33fd0c1c43202ba46e9e24ba403d28d7a4
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-22 11:32:26 +0100

    add user mocks for testing

---
+++ src/mocks/users-mock.ts
@@ -0,0 +1,22 @@
+export const mockedUsers = [
+  {
+    _id: '6415cfa3b275f174bfbe76ec',
+    email: 'lorenadiaz@gmail.com',
+    firstName: 'Lorena',
+    lastName: 'Anaya',
+    phone: '626857375',
+    languages: 'Chinese, Spanish, English',
+    password: '68684f90e1f455e56f3948385c705484',
+    translations: [],
+  },
+  {
+    _id: '64189874025eb95b21f811f3',
+    email: 'demo@demo.es',
+    firstName: 'alex',
+    lastName: 'aaaa',
+    phone: '666666666',
+    languages: 'es',
+    password: 'a6e7eae1c2e69cda6ba759f8b77beda0',
+    translations: ['6419def3e4b5484f21e5f7e2'],
+  },
+];
