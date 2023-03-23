125e50a75449606b3cb51d35c0261574e0e847f5
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-23 16:25:40 +0100

    make translation details card dynamic

--- src/features/translations/TranslationDetailsCard/TranslationDetailsCard.tsx
+++ src/features/translations/TranslationDetailsCard/TranslationDetailsCard.tsx
@@ -1,4 +1,8 @@
+import { useAppSelector } from '../../../app/hooks';
+import { Translation } from '../../../shared/models/translation-model';
+import { selectTranslation } from '../translations-slice';
 import {
+  DownloadLink,
   InfoContainer,
   Property,
   PropertyInfo,
@@ -7,48 +11,73 @@ import {
 } from './TranslationDetailsCardStyled';
 
 export const TranslationDetailsCard = () => {
+  const translation: Translation = useAppSelector(selectTranslation);
+
+  const dueDate = new Date(translation.dueDate);
+  const day = dueDate.getDate();
+  const month = dueDate.getMonth();
+  const year = dueDate.getFullYear();
   return (
     <>
       <TranslationCardContainer>
         <TranslationDetailsCardStyled>
           <InfoContainer>
             <Property role="paragraph">Booking ref.</Property>
-            <PropertyInfo role="paragraph">TS1</PropertyInfo>
+            <PropertyInfo role="paragraph">
+              {translation.bookingRef}
+            </PropertyInfo>
           </InfoContainer>
 
           <InfoContainer>
             <Property role="paragraph">Due date:</Property>
-            <PropertyInfo role="paragraph">24/03/2023</PropertyInfo>
+            <PropertyInfo role="paragraph">
+              {` ${day}/${month}/${year}`}
+            </PropertyInfo>
           </InfoContainer>
 
           <InfoContainer>
             <Property role="paragraph">Source:</Property>
-            <PropertyInfo role="paragraph">English</PropertyInfo>
+            <PropertyInfo role="paragraph">
+              {translation.languageFrom}
+            </PropertyInfo>
           </InfoContainer>
 
           <InfoContainer>
             <Property role="paragraph">Target:</Property>
-            <PropertyInfo role="paragraph">Spanish</PropertyInfo>
+            <PropertyInfo role="paragraph">
+              {translation.languageTo}
+            </PropertyInfo>
           </InfoContainer>
 
           <InfoContainer>
             <Property role="paragraph">Nº of words:</Property>
-            <PropertyInfo role="paragraph">574</PropertyInfo>
+            <PropertyInfo role="paragraph">{translation.words}</PropertyInfo>
           </InfoContainer>
 
           <InfoContainer>
             <Property role="paragraph">Status:</Property>
-            <PropertyInfo role="paragraph">Pending</PropertyInfo>
+            <PropertyInfo role="paragraph">{translation.status}</PropertyInfo>
           </InfoContainer>
 
           <InfoContainer>
             <Property role="paragraph">Doc to translate:</Property>
-            <PropertyInfo role="paragraph">Download</PropertyInfo>
+            <PropertyInfo role="paragraph">
+              <DownloadLink
+                href="https://kfgqypbospvevemrysdv.supabase.co/storage/v1/object/public/translations/PRUEBA1.pdf"
+                download
+                target="_blank"
+                rel="noreferrer"
+              >
+                Download
+              </DownloadLink>
+            </PropertyInfo>
           </InfoContainer>
 
           <InfoContainer>
             <Property role="paragraph">Doc translated:</Property>
-            <PropertyInfo role="paragraph">Upload</PropertyInfo>
+            <PropertyInfo role="paragraph">
+              {translation.translatedDoc}
+            </PropertyInfo>
           </InfoContainer>
         </TranslationDetailsCardStyled>
       </TranslationCardContainer>
