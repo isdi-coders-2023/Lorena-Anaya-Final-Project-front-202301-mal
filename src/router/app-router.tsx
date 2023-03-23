1814f5a3dacbbc9a76e4049281f213e678f4fad9
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-23 16:26:49 +0100

    add not found page in app router

--- src/router/app-router.tsx
+++ src/router/app-router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../layout/MainLayout';
 import { DashboardPage } from '../pages/DasboardPage/DashboardPage';
+import { NotFoundPage } from '../pages/error-pages/404Page/404Page';
 import { LoginPage } from '../pages/LoginPage/LoginPage';
 import { RegisterPage } from '../pages/RegisterPage/RegisterPage';
 import { TranslationDetailsPage } from '../pages/TranslationDetailsPage/TranslationDetailsPage';
@@ -10,28 +11,34 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <LoginPage />,
+    errorElement: <NotFoundPage />,
   },
   {
     path: '/register',
     element: <RegisterPage />,
+    errorElement: <NotFoundPage />,
   },
 
   {
     path: '/main',
     element: <MainLayout />,
+    errorElement: <NotFoundPage />,
 
     children: [
       {
         path: '/main/dashboard',
         element: <DashboardPage />,
+        errorElement: <NotFoundPage />,
       },
       {
         path: '/main/create',
         element: <TranslationFormPage />,
+        errorElement: <NotFoundPage />,
       },
       {
         path: '/main/details',
         element: <TranslationDetailsPage />,
+        errorElement: <NotFoundPage />,
       },
     ],
   },
