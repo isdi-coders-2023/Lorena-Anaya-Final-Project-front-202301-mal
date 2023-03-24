b8b6ad0cf965da339d6acc3e7f36df054ba411b7
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-24 18:08:50 +0100

    add translations details card and page tests

--- src/pages/TranslationDetailsPage/TranslationDetailsPage.test.tsx
+++ src/pages/TranslationDetailsPage/TranslationDetailsPage.test.tsx
@@ -1,21 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from '../../app/store';
+import { screen } from '@testing-library/react';
+
 import { MemoryRouter } from 'react-router-dom';
 import { TranslationDetailsPage } from './TranslationDetailsPage';
 import React from 'react';
+import { renderWithProviders } from '../../mocks/test-util';
 
-test('When rendering the details translation page, 16 paragraphs should be displayed', () => {
-  render(
+test('When rendering the details translation page, a heading should be displayed', () => {
+  renderWithProviders(
     <MemoryRouter>
       <React.StrictMode>
-        <Provider store={store}>
-          <TranslationDetailsPage />
-        </Provider>
+        <TranslationDetailsPage />
       </React.StrictMode>
     </MemoryRouter>,
   );
 
-  const infoElements = screen.getAllByRole('paragraph');
-  expect(infoElements.length).toBe(16);
+  const heading = screen.getAllByRole('heading');
+  expect(heading.length).toBe(1);
 });
