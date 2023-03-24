363fea85ba086c239dc0f8b2837f8ae240a38aa9
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-24 18:07:24 +0100

    add translation card details component with functionality

--- src/features/translations/TranslationDetailsCard/TranslationDetailsCard.tsx
+++ src/features/translations/TranslationDetailsCard/TranslationDetailsCard.tsx
@@ -1,6 +1,14 @@
-import { useAppSelector } from '../../../app/hooks';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { NotFoundPage } from '../../../pages/error-pages/404Page/404Page';
 import { Translation } from '../../../shared/models/translation-model';
-import { selectTranslation } from '../translations-slice';
+import {
+  getTranslationsByIdAsync,
+  selectapiStatus,
+  selectTranslation,
+} from '../translations-slice';
+import { TranslationsFeedbackComponent } from '../TranslationsList/TranslationsListStyled';
 import {
   DownloadLink,
   InfoContainer,
@@ -13,74 +21,95 @@ import {
 export const TranslationDetailsCard = () => {
   const translation: Translation = useAppSelector(selectTranslation);
 
+  const apiStatus = useAppSelector(selectapiStatus);
+
+  const dispatch = useAppDispatch();
+
+  let { translationId } = useParams();
+
+  useEffect(() => {
+    dispatch(getTranslationsByIdAsync(translationId as string));
+  }, [dispatch, translationId]);
+
   const dueDate = new Date(translation.dueDate);
   const day = dueDate.getDate();
   const month = dueDate.getMonth();
   const year = dueDate.getFullYear();
-  return (
-    <>
-      <TranslationCardContainer>
-        <TranslationDetailsCardStyled>
-          <InfoContainer>
-            <Property role="paragraph">Booking ref.</Property>
-            <PropertyInfo role="paragraph">
-              {translation.bookingRef}
-            </PropertyInfo>
-          </InfoContainer>
 
-          <InfoContainer>
-            <Property role="paragraph">Due date:</Property>
-            <PropertyInfo role="paragraph">
-              {` ${day}/${month}/${year}`}
-            </PropertyInfo>
-          </InfoContainer>
+  if (apiStatus === 'idle' && translation._id !== undefined) {
+    return (
+      <>
+        <TranslationCardContainer>
+          <TranslationDetailsCardStyled>
+            <InfoContainer>
+              <Property role="paragraph">Booking ref.</Property>
+              <PropertyInfo role="paragraph">
+                {translation.bookingRef}
+              </PropertyInfo>
+            </InfoContainer>
 
-          <InfoContainer>
-            <Property role="paragraph">Source:</Property>
-            <PropertyInfo role="paragraph">
-              {translation.languageFrom}
-            </PropertyInfo>
-          </InfoContainer>
+            <InfoContainer>
+              <Property role="paragraph">Due date:</Property>
+              <PropertyInfo role="paragraph">
+                {` ${day}/${month}/${year}`}
+              </PropertyInfo>
+            </InfoContainer>
 
-          <InfoContainer>
-            <Property role="paragraph">Target:</Property>
-            <PropertyInfo role="paragraph">
-              {translation.languageTo}
-            </PropertyInfo>
-          </InfoContainer>
+            <InfoContainer>
+              <Property role="paragraph">Source:</Property>
+              <PropertyInfo role="paragraph">
+                {translation.languageFrom}
+              </PropertyInfo>
+            </InfoContainer>
 
-          <InfoContainer>
-            <Property role="paragraph">Nº of words:</Property>
-            <PropertyInfo role="paragraph">{translation.words}</PropertyInfo>
-          </InfoContainer>
+            <InfoContainer>
+              <Property role="paragraph">Target:</Property>
+              <PropertyInfo role="paragraph">
+                {translation.languageTo}
+              </PropertyInfo>
+            </InfoContainer>
 
-          <InfoContainer>
-            <Property role="paragraph">Status:</Property>
-            <PropertyInfo role="paragraph">{translation.status}</PropertyInfo>
-          </InfoContainer>
+            <InfoContainer>
+              <Property role="paragraph">Nº of words:</Property>
+              <PropertyInfo role="paragraph">{translation.words}</PropertyInfo>
+            </InfoContainer>
 
-          <InfoContainer>
-            <Property role="paragraph">Doc to translate:</Property>
-            <PropertyInfo role="paragraph">
-              <DownloadLink
-                href="https://kfgqypbospvevemrysdv.supabase.co/storage/v1/object/public/translations/PRUEBA1.pdf"
-                download
-                target="_blank"
-                rel="noreferrer"
-              >
-                Download
-              </DownloadLink>
-            </PropertyInfo>
-          </InfoContainer>
+            <InfoContainer>
+              <Property role="paragraph">Status:</Property>
+              <PropertyInfo role="paragraph">{translation.status}</PropertyInfo>
+            </InfoContainer>
 
-          <InfoContainer>
-            <Property role="paragraph">Doc translated:</Property>
-            <PropertyInfo role="paragraph">
-              {translation.translatedDoc}
-            </PropertyInfo>
-          </InfoContainer>
-        </TranslationDetailsCardStyled>
-      </TranslationCardContainer>
-    </>
-  );
+            <InfoContainer>
+              <Property role="paragraph">Doc to translate:</Property>
+              <PropertyInfo role="paragraph">
+                <DownloadLink
+                  href={translation.toTranslateDoc}
+                  download
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Download
+                </DownloadLink>
+              </PropertyInfo>
+            </InfoContainer>
+
+            <InfoContainer>
+              <Property role="paragraph">Doc translated:</Property>
+              <PropertyInfo role="paragraph">
+                {translation.translatedDoc}
+              </PropertyInfo>
+            </InfoContainer>
+          </TranslationDetailsCardStyled>
+        </TranslationCardContainer>
+      </>
+    );
+  } else if (apiStatus === 'loading') {
+    return (
+      <TranslationsFeedbackComponent>
+        {`Loading your translation details...`}
+      </TranslationsFeedbackComponent>
+    );
+  } else {
+    return <NotFoundPage />;
+  }
 };
